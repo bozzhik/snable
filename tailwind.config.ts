652cbe8b61885722d215e47ce9530cf184b00856
maxim <bozzhik@ya.ru>
2025-02-01 01:54:32 +0300

    import `SF Pro Display` font using custom `CDN`
    https://snable.website/api/font?weights=400

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
 import type {Config} from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
   theme: {
+    fontFamily: {
+      sans: ['SF Pro Display', ...defaultTheme.fontFamily.sans],
+    },
+    fontWeight: {
+      normal: '400',
+    },
     colors: {
       background: '#121212',
       unit: '#191919',
