8b21949cc7af11af84ea58982c807171752d0ece
maxim <bozzhik@ya.ru>
2025-01-31 19:49:24 +0300

    add `tailwindcss` for styling
    https://tailwindcss.com/docs/installation/using-postcss

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Popup} from './Popup'
 
+import '@/globals.css'
+
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
     <Popup />
