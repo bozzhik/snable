1b6c9800a2e538e6c3daebb85c5f691666d5c77e
maxim <bozzhik@ya.ru>
2025-02-01 01:12:38 +0300

    add `config` file for `tailwindcss`
    https://tailwindcss.com/docs/functions-and-directives#config-directive (v4*)

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type {Config} from 'tailwindcss'
+
+export default {
+  theme: {
+    colors: {
+      background: '#121212',
+      unit: '#191919',
+      control: '#1F1F1F',
+      white: '#FFFFFF',
+      gray: '#818181',
+      transparent: 'transparent',
+    },
+  },
+  plugins: [],
+} satisfies Config
