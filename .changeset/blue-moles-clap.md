ec173f98856ac10bbaff9695c4d80f38d0793023
maxim <bozzhik@ya.ru>
2025-02-14 17:52:23 +0300

    pass `GET_TAB_INFO` data into `Header` Global component

---
+++ .changeset/blue-moles-clap.md
@@ -0,0 +1,5 @@
+---
+'snable': patch
+---
+
+pass `GET_TAB_INFO` data into `Header` Global component
--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,13 +1,43 @@
+import {type TabInfo} from '_bg/getTabData'
 import {BOX_STYLES} from '~/Global/Container'
+
+import {useEffect, useState} from 'react'
 import {cn} from '@/lib/utils'
 
 import Button from '~/UI/Button'
 
 export default function Header() {
+  const [tabData, setTabData] = useState<TabInfo>({
+    favicon: null,
+    url: '',
+    title: '',
+  })
+
+  useEffect(() => {
+    chrome.runtime.sendMessage({type: 'GET_TAB_INFO'}, (response) => {
+      setTabData(response)
+    })
+  }, [])
+
   return (
     <div className={cn(BOX_STYLES, 'py-3 flex justify-between border-b-3 border-control')}>
       <div className={cn('size-10 bg-control rounded-lg p-2', 'group')}>
         <div className={cn('size-full bg-white rounded-full', 'group-hover:scale-[1.1] group-hover:bg-white/80 duration-300')}></div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <div className="size-5 flex items-center justify-center rounded-sm bg-gray-200">
+          {tabData.favicon ? (
+            <img src={tabData.favicon} alt="Favicon" className="size-full rounded-sm" />
+          ) : (
+            <span className="text-xs text-gray-500">🌐</span> // Placeholder icon
+          )}
+        </div>
+
+        <div>
+          {tabData.title && <p className="text-sm font-medium">{tabData.title}</p>}
+          {tabData.url && <p className="text-xs text-gray-500">{tabData.url}</p>}
+        </div>
       </div>
 
       <Button variant="secondary">Button</Button>
